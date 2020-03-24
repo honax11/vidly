abca9e36ddba6dc392040fb3c4b93b4c9cccbe3d
DESKTOP-PE94F06\Sergo <ser.koshevoy@gmail.com>
2020-03-24 17:40:53 +0200

    Add project files.

---
+++ vidly/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        
+
+    
+    }
+}
